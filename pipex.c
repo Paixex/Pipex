fab333ecdf98fc8c906033dc3def5eb41bd8f6e1
Paixex <puto.gil.18@gmail.com>
2023-05-09 19:20:56 +0100

    command 2 missing

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: digil-pa <digil-pa@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 17:12:21 by digil-pa          #+#    #+#             */
-/*   Updated: 2023/05/07 17:48:32 by digil-pa         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:59:13 by digil-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int ac, char **av, char **envp)
 	if (ac == 5)
 		pipex(av, envp);
 	else
-		erros(0);
+		errors(0);
 	return(0);
 }
 
@@ -27,9 +27,31 @@ void pipex(char **av, char **envp)
 	int	pipe_fd[2];
 
 	if (pipe(pipe_fd))
-		erros(1);
+		errors(1);
 	pipe_id = fork();
 	if (pipe_id < 0)
-		erros(2);
+		errors(2);
 	if (!pipe_id)
+}
+
+void	first(char **envp, char **av, int *pipefd)
+{
+	char *str;
+	char **avsplit;
+	int infile;
+
+	close(pipefd[0]);
+	infile = open(av[1], O_RDONLY)
+	if (infile < 0)
+		errors(3);
+	if (dup2(infile, STDIN_FILENO) < 0)
+		errors(6);
+	if (dup2(infile, STDOUT_FILENO) < 0)
+		errors(6);
+	avsplit = ft_split(av[2], ' ');
+	str = getpath(envp, avsplit[0]);
+	execve(str, avsplit, envp);
+	get_free(avsplit);
+	free(str);
+	exit(4);
 }
